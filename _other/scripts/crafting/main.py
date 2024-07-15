6034846078cd6bf701131a4b9e0487a828de56af
asdru22 <76167257+asdru22@users.noreply.github.com>
2024-07-15 23:22:31 +0200

    recipes

--- _other/scripts/crafting/main.py
+++ _other/scripts/crafting/main.py
@@ -1,18 +1,20 @@
 import json
 root = "C:/Users/Ale/Documents/GitHub/Cognition/"
-write_to = root+"datapack/data/cgn/functions/recipes/shaped.mcfunction"
-loot_table_path = root+"datapack/data/cgn/loot_tables/technical/count/"
+write_to = root+"datapack/data/cgn/function/recipe/shaped.mcfunction"
 
-def make_loot_table(count,id,type):
-    with open(loot_table_path+f"{id}_{count}.json","w") as loot_table:
-        loot_table.write(json.dumps({"pools":[{"rolls":1,"entries":[{"type":"minecraft:loot_table","name":f"cgn:{type}s/{id}","functions":[{"function":"minecraft:set_count","count":count}]}]}]}))
-        
-out = "#"*50+ "\n##\tAutomagically generated using Asdrucorp Scripts\n"+"#"*50
+
+def count_loot_table(type,id,count):
+    return json.dumps({"pools":[{"rolls":1,"entries":[{"type":"minecraft:loot_table","value":f"cgn:{type}s/{id}"}],"functions":[{"function":"minecraft:set_count","count":count}]}]})
+
+
 with open(root+"_other/scripts/crafting/recipes.json") as recipes:
+    out = "#"*50+ "\n##\tAutomagically generated using Asdrucorp Scripts\n"+"#"*50
     recipes = json.loads(recipes.read())
+
     for key,value in recipes.items():
         out += "\nexecute store result score @s smithed.data if entity @s[scores={smithed.data=0}] if data storage smithed.crafter:input recipe{"
         col = 0
+
         for row in value["recipe"]:
             if not row == []:
                 out +=f"{col}:["
@@ -23,7 +25,12 @@ with open(root+"_other/scripts/crafting/recipes.json") as recipes:
                     elif not ":" in item:
                         out += "{Slot:"+str(slot)+"b,id:\"minecraft:"+item+"\"}"
                     elif "cgn:" in item:
-                        out += "{Slot:"+str(slot)+"b,tag:{cgn:{id:\""+item[4:]+"\"}}}"
+                        out += json.dumps(
+                            {"Slot":f"{slot}b",
+                             "components":{
+                                "minecraft:item_name":"{\"translate\":\"item.cgn."+item[4:]+"\"}"
+                            }
+                        })
                     slot +=1
                     if not slot == 3: out +=","
                 out += "],"
@@ -40,15 +47,15 @@ with open(root+"_other/scripts/crafting/recipes.json") as recipes:
   
         out +="} run "
         if value["type"] == "block" or value["type"] == "item":
-            out+= "loot replace block ~ ~ ~ container.16 loot cgn:"
+            out+= "loot replace block ~ ~ ~ container.16 loot "
             if not "count" in value:
-                out+=value["type"]+"s/"+key
+                out+="cgn:"+value["type"]+"s/"+key
             else:
-                out += f'technical/count/{key}_{value["count"]}'
-                make_loot_table(count=value["count"],id=key,type=value["type"])
+                out+=count_loot_table(value["type"],key,value["count"])
         elif value["type"] == "function":
             out +="function cgn:"+key
         
-    
+
+
 with open(write_to,"w") as f:
     f.write(out)
