6591e70bd4267ea762ff2a88cb93bc6f53dc581f
asdru22 <76167257+asdru22@users.noreply.github.com>
2024-09-13 17:04:21 +0200

    g

--- z_other/scripts/convert/main.py
+++ z_other/scripts/convert/main.py
@@ -1,12 +1,47 @@
 import os
 import json
 
-namespace = "cgn"
-footer = {"translate":"pack.cgn","color":"#653AA0","italic":False}
+def get_name(name):
+    return {
+        "function": "minecraft:set_name",
+        "target": "item_name",
+        "name": {
+            "translate": f"item.cgn.{name}"
+        }
+    }
 
-def process_file(name,data):
-    print(data)
+def get_lore(): 
+    return {
+        "function": "minecraft:set_lore",
+        "lore": [
+            {
+                "translate": "pack.cgn",
+                "color": "#653AA0",
+                "italic": False
+            }
+        ],
+        "mode": "replace_all"
+    }
 
+def process_file(name, data,file_path):
+    print(name)
+    root = data["pools"][0]["functions"]
+
+    # Loop over the list of functions and remove the item_name and lore functions
+    root[:] = [func for func in root if not (
+        "set_components" in func and
+        (
+            "minecraft:item_name" in func["set_components"] or
+            "minecraft:lore" in func["set_components"]
+        )
+    )]
+
+    # Add the new functions for name and lore
+    root.append(get_name(name))
+    root.append(get_lore())
+    
+    with open(file_path, 'w', encoding='utf-8') as json_file:
+      json.dump(data, json_file, indent=2)
 
 def read_json_files(subfolder):
     # Get a list of all files in the subfolder
@@ -18,29 +53,12 @@ def read_json_files(subfolder):
             with open(file_path, 'r', encoding='utf-8') as json_file:
                 try:
                     data = json.load(json_file)
-                    process_file(os.path.splitext(filename)[0],data)
+                    process_file(os.path.splitext(filename)[0], data,file_path)
                 except json.JSONDecodeError:
                     print(f"Error decoding {filename}")
 
-output_path = 'datapack/data/cgn/loot_table/items'
-items_path = 'datapack/data/cgn/loot_table/items'
-read_json_files(items_path)
+# Define paths
+path = 'datapack/data/cgn/loot_table/items'
 
-name = {
-          "function": "minecraft:set_name",
-          "target": "item_name",
-          "name": {
-            "translate": f"item.cgn.{filename}"
-          }
-        }
-lore = {
-          "function": "minecraft:set_lore",
-          "lore": [
-            {
-              "translate": "pack.cgn",
-              "color": "#653AA0",
-              "italic": False
-            }
-          ],
-          "mode": "replace_all"
-        },
+# Call the function
+read_json_files(path)
